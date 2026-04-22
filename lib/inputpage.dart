f5f1c1ff948d4ba09634f399f0281d05d613993a
youssefzouari <149727808+youssefzouari@users.noreply.github.com>
2026-04-22 22:24:53 +0100

    third commit

--- lib/inputpage.dart
+++ lib/inputpage.dart
@@ -3,39 +3,41 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusiable_card.dart';
 import 'icon_content.dart';
 
-const activereusiablecardcolor = Color(0xFF1d1E33);
-const passifreusiablecardcolor = Color(0xFF111328);
+const activereusiablecardcolor = Color(0xFF111328);
+const passifreusiablecardcolor = Color(0xFF1d1E33);
 const bottomcontainerheight = 80.0;
 const bottomcontainercolor = Color(0xFFEB1555);
-enum Gender {male ,female}
+
+enum Gender { male, female }
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
-  Color malecardcolor = passifreusiablecardcolor;
-  Color femalecardcolor = passifreusiablecardcolor;
-  void updatecolor(Gender gender) {
-    if (gender == Gender.male) {
-      femalecardcolor=passifreusiablecardcolor;
-      if (malecardcolor == passifreusiablecardcolor) {
-        malecardcolor = activereusiablecardcolor;
-      } else {
-        malecardcolor = passifreusiablecardcolor;
-      }
-
-    }
-    if (gender == Gender.female) {
-      malecardcolor=passifreusiablecardcolor;
-      if (femalecardcolor == passifreusiablecardcolor) {
-        femalecardcolor = activereusiablecardcolor;
-      } else {
-        femalecardcolor = passifreusiablecardcolor;
-      }
-    }
-  }
-
+  //Color malecardcolor = passifreusiablecardcolor;
+  //Color femalecardcolor = passifreusiablecardcolor;
+  //void updatecolor(Gender gender) {
+  //  if (gender == Gender.male) {
+  //    femalecardcolor=passifreusiablecardcolor;
+  //    if (malecardcolor == passifreusiablecardcolor) {
+  //      malecardcolor = activereusiablecardcolor;
+  //    } else {
+  //      malecardcolor = passifreusiablecardcolor;
+  //    }
+  //
+  //  }
+  //  if (gender == Gender.female) {
+  //    malecardcolor=passifreusiablecardcolor;
+  //    if (femalecardcolor == passifreusiablecardcolor) {
+  //      femalecardcolor = activereusiablecardcolor;
+  //    } else {
+  //      femalecardcolor = passifreusiablecardcolor;
+  //    }
+  //  }
+  //}
+  Gender? selectedgender;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,11 +55,13 @@ class _InputPageState extends State<InputPage> {
                   child: GestureDetector(
                     onTap: () {
                       setState(() {
-                        updatecolor(Gender.male);
+                        selectedgender = Gender.male;
                       });
                     },
                     child: ReusableCard(
-                      colour: malecardcolor,
+                      colour: selectedgender == Gender.male
+                          ? activereusiablecardcolor
+                          : passifreusiablecardcolor,
                       cardchild: iconcontent(
                         iconname: (FontAwesomeIcons.mars),
                         gender: "Male",
@@ -69,11 +73,13 @@ class _InputPageState extends State<InputPage> {
                   child: GestureDetector(
                     onTap: () {
                       setState(() {
-                        updatecolor(Gender.female);
+                        selectedgender = Gender.female;
                       });
                     },
                     child: ReusableCard(
-                      colour: femalecardcolor,
+                      colour: selectedgender == Gender.female
+                          ? activereusiablecardcolor
+                          : passifreusiablecardcolor,
                       cardchild: iconcontent(
                         iconname: (FontAwesomeIcons.venus),
                         gender: "Female",
